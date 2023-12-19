82e7793cc4ced1b9dc00dd6414c47262144956db
Jetter Garcia <102631417+jetsgarcia@users.noreply.github.com>
2023-12-20 07:45:16 +0800

    Added microanimation for about me section

--- week3/script.js
+++ week3/script.js
@@ -1,1 +1,11 @@
+// For the sliding animation of elements
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
 
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((element) => observer.observe(element));
