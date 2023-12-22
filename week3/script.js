d0107bf610f71f8561a451b68f760dd400e828fd
Jetter Garcia <102631417+jetsgarcia@users.noreply.github.com>
2023-12-22 08:50:22 +0800

    Added effects on the japanese text

--- week3/script.js
+++ week3/script.js
@@ -1,3 +1,65 @@
+// For the text effect
+const japaneseAlphabet =
+  "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+const englishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let iterations = 0;
+let timeoutId;
+let isJapanese = true;
+
+document.querySelector(".japanese").onmouseover = (event) => {
+  clearTimeout(timeoutId);
+
+  const translate = (letter, alphabet) => {
+    const index = alphabet.indexOf(letter);
+    return index !== -1 ? englishAlphabet[index] : letter;
+  };
+
+  const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iterations) {
+          return translate(event.target.dataset.value[index], japaneseAlphabet);
+        }
+
+        if (isJapanese) {
+          return japaneseAlphabet[Math.floor(Math.random() * 46)];
+        } else {
+          return englishAlphabet[Math.floor(Math.random() * 26)];
+        }
+      })
+      .join("");
+
+    if (iterations >= event.target.dataset.value.length) {
+      clearInterval(interval);
+
+      timeoutId = setTimeout(() => {
+        const hackerInterval = setInterval(() => {
+          event.target.innerText = Array.from(
+            { length: event.target.dataset.value.length },
+            () => japaneseAlphabet[Math.floor(Math.random() * 46)]
+          ).join("");
+        }, 100);
+
+        setTimeout(() => {
+          clearInterval(hackerInterval);
+          event.target.innerText = "こんにちは";
+          iterations = 0;
+        }, 2000);
+      }, 2000);
+    }
+
+    // change to false if the text is already in english
+    if (isJapanese) {
+      isJapanese = false;
+    } else {
+      isJapanese = true;
+    }
+
+    iterations += 1 / 3;
+  }, 30);
+};
+
 // For the moving eyes
 document.addEventListener("mousemove", (e) => {
   const mousex = e.clientX;
