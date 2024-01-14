cadf175694895e0971cbc994af52254e69faa4ce
Jetter Garcia <102631417+jetsgarcia@users.noreply.github.com>
2024-01-14 14:19:35 +0800

    Styled the navigation links for the navbar

--- week5/script.js
+++ week5/script.js
@@ -4,9 +4,9 @@
   Author: Jetter Garcia
   Created: 01/13/2024 [MM/DD/YYYY]
 
-  Note: By the time I wrote this code, God and I knew how it worked. Now only God knows.
+  Note: By the time I wrote this code, God and I knew how it worked. Now, only God knows.
 
-  Hours wasted coding: 0
+  Hours wasted coding: 2
 
   Dependencies:
   - None
@@ -15,22 +15,33 @@
 
 // Initialization variables for theme
 let prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
-let theme = ["light-mode", "dark-mode"];
+let navLinks = ["#home-link", "#about-link", "#portfolio-link"];
+// let theme = ["light-mode", "dark-mode"];
 
-// Initializations variables for navigation
+// Initializations variables for navigation contents to display
 let navigations = ["home", "about", "portfolio"];
 
 function changePage(index) {
   // We loop through each page in the 'navigations' array.
   navigations.forEach((page) => {
     // For each page, we get the corresponding HTML element by its ID.
-    var pageElement = document.getElementById(page);
+    let pageElement = document.getElementById(page);
 
     // We then check if the current page in the loop is the one we want to navigate to.
     // If it is, we set its display style to "block", making it visible.
     // If it's not, we set its display style to "none", hiding it.
     pageElement.style.display = navigations[index] === page ? "block" : "none";
   });
-}
 
-document.addEventListener("DOMContentLoaded", () => console.log("DOM loaded"));
+  navLinks.forEach((link) => {
+    // For each link, we get the corresponding HTML element by its ID
+    let linkElement = document.querySelector(link);
+
+    // We then check if the current link in the loop is the one we want to navigated to.
+    if (navLinks[index] === link) {
+      linkElement.classList.add("active");
+    } else {
+      linkElement.classList.remove("active");
+    }
+  });
+}
