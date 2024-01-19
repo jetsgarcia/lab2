46231b30e5b551850097104e56bcb966762c0e71
Jetter Garcia <102631417+jetsgarcia@users.noreply.github.com>
2024-01-19 18:47:58 +0800

    Added the hero section of the homepage

--- week5/script.js
+++ week5/script.js
@@ -85,3 +85,31 @@ function switchTheme(e) {
     toggleSwitch.checked = true;
   }
 }
+
+// For the moving eyes
+document.addEventListener("mousemove", (e) => {
+  const mousex = e.clientX;
+  const mousey = e.clientY;
+
+  const anchor = document.querySelector("#anchor");
+  const rect = anchor.getBoundingClientRect();
+  const anchorX = rect.left + rect.width / 2;
+  const anchorY = rect.top + rect.height / 2;
+
+  const eye = document.querySelector("#eye");
+  const eyeMaxMove = 10;
+
+  // Calculate distance from center of anchor to mouse position
+  const dx = mousex - anchorX;
+  const dy = mousey - anchorY;
+
+  // Calculate angle of that distance
+  const angle = Math.atan2(dy, dx);
+
+  // Calculate new position for eye based on maximum allowed movement
+  const eyeX = Math.cos(angle) * eyeMaxMove;
+  const eyeY = Math.sin(angle) * eyeMaxMove;
+
+  // Apply new position to eye
+  eye.style.transform = `translate(${eyeX / 16}rem, ${eyeY / 16}rem)`;
+});
